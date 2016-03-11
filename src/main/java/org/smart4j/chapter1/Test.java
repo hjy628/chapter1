4763b3b7f7ec117e93982c2aeaccb63326f93cdc
hjy628 <hjy628@gmail.com>
2016-03-11 15:12:04 +0800

    测试第一次提交到git

---
+++ src/main/java/org/smart4j/chapter1/Test.java
@@ -0,0 +1,30 @@
+package org.smart4j.chapter1;
+
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by hjy on 16-3-10.
+ */
+public class Test {
+    public static void main(String[] args) {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    TimeUnit.MINUTES.sleep(10);
+                    System.out.println("一分钟到了");
+                    System.out.println(new Date());
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+            }
+        },"测试");
+      //  thread.setDaemon(true);
+        System.out.println(new Date());
+        thread.start();
+
+
+
+    }
+}
